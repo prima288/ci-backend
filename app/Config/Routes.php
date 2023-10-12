7997f821bbe5fcaedd534c4898eb2c772ed42083
prima288 <kembangprima122@gmail.com>
2023-10-12 15:51:03 +0700

    insert, create, update, and delete

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('insertproducts', 'ProductController::insertProduct');
+$routes->post('insertproducts', 'ProductController::insertProduct');
+$routes->get('products', 'ProductController::readProduct');
+$routes->get('edit-product/(:any)', 'ProductController::getProduct/$1');
+$routes->post('update-product/(:any)', 'ProductController::updateProduct/$1');
+$routes->get('delete-product/(:any)', 'ProductController::deleteProduct/$1');
---
+++ app/Controllers/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Controllers;
+use App\Controllers\BaseController;
+use App\Models\ProductModel;
+
+class ProductController extends BaseController {
+
+    private $product;
+
+    public function __construct(){
+        $this->product = new ProductModel();
+    }
+    
+    public function insertProduct(){
+    if ($this->request->getMethod() === 'post') {
+        $nama_product = $this->request->getVar("nama_product");
+        $description = $this->request->getVar("description");
+
+        // Lakukan validasi atau operasi lain sesuai kebutuhan
+
+        // Setelah validasi atau operasi lainnya, Anda dapat menyimpan data ke database
+        $data = [
+            'nama_product' => $nama_product,
+            'description' => $description
+        ];
+        $this->product->insertProductORM($data);
+
+        // Redirect ke halaman lain atau tampilkan pesan sukses
+        return redirect()->to(base_url("products"));
+    } else {
+        // Jika bukan metode POST, tampilkan formulir
+        return view('insert_product');
+    }
+}
+
+
+    public function readProduct(){
+        $products = $this->product->findAll();
+        $data = [
+            'data' => $products
+        ];
+
+        return view('product', $data);
+    }
+    public function getProduct($id){
+        $product = $this->product->where('id', $id)->first();
+        $data = [
+            'product' => $product
+            ];
+        return view('edit_product', $data);
+    }
+    public function updateProduct($id){
+        $nama_product = $this->request->getVar("nama_product");
+        $description = $this->request->getVar("description");
+        $data = [
+            'nama_product' => $nama_product,
+            'description' => $description
+        ];
+        $this->product->update($id, $data);
+        return redirect()->to(base_url("products"));
+    }
+    public function deleteProduct($id){
+        $this->product->delete($id);
+        return redirect()->to(base_url("products"));
+    }
+    
+}
